71a5f7db65c0d89c0066840f3ae9ca25983e3102
Bruno Gonçalves Pereira <bruno.g.p897@gmail.com>
2021-07-27 01:16:36 -0300

    v1.0 app

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const config = require('./config')
+const path = require('path') 
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        with: 1280,
+        height: 720,
+        titleBarStyle: 'hidden',
+        alwaysOnTop: true,
+        webPreferences:{
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+
+    
+    /*  URL dinâmica 
+    
+        win.loadURL(config.url) 
+        
+    */
+
+    /* Posso dar load em uma URL ao invés de um arquivo .hmtl 
+        
+        win.loadURL('https://gestordepedidos.ifood.com.br/#/login') 
+    */
+}
+
+function toggleDevTools() {
+    win.webContents.toggleDevTools()
+}
+
+function createShortcurts() {
+    globalShortcut.register('CmdOrCtrl+J', toggleDevTools)
+}
+
+app.whenReady()
+
+.then(() => {
+    createWindow()
+    createShortcurts()
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+  
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit()
+})
